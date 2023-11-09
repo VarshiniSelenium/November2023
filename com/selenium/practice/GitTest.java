52ef22019954bfd3a885a41584b0193513c7114c
VarshiniSelenium <tallurivarshini07@gmail.com>
2023-11-09 14:43:18 +0530

    Added successfully

---
+++ com/selenium/practice/GitTest.java
@@ -0,0 +1,14 @@
+package com.selenium.practice;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=1;
+		int b=12;
+		int c= a+b;
+		System.out.println(c);	
+		}
+
+}
---
+++ src/com/selinium/gitTest/gittest.java
@@ -0,0 +1,11 @@
+package com.selinium.gitTest;
+
+public class gittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+	}
+
+}
