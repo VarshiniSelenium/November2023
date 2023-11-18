1dc434833e79a5936ae3954636cfc8b1ed5fb875
VarshiniSelenium <tallurivarshini07@gmail.com>
2023-11-18 11:10:46 +0530

    Added Helloworld1

---
+++ com/selenium/practice/Helloworld1.java
@@ -0,0 +1,14 @@
+package com.selenium.practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=1;
+		int c= a*b;
+		System.out.println(c);
+	}
+
+}
